4f69761dab7ce8a0ed11659b859ff65d967a5106
markohautala <markohautala@live.se>
2025-01-22 07:18:22 +0100

    add favicon and update page title in layout component

--- next-app/app/layout.jsx
+++ next-app/app/layout.jsx
@@ -5,6 +5,8 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <title>PIXSHARE - Created by Marko Hautala</title>
+        <link rel="icon" href="/images/favicon.png" type="image/png" />
         <link
           rel="stylesheet"
           href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
